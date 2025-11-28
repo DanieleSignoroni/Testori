6348d28a29d713c5b555097b9d22a1b97afad1a9
Daniele aka COBRA <daniele.signoroni@softre.it>
2025-11-28 12:29:04 +0100

    Update EasyCheckService.java

--- 5.1/src/it/testori/thip/base/generale/api/EasyCheckService.java
+++ 5.1/src/it/testori/thip/base/generale/api/EasyCheckService.java
@@ -2,18 +2,25 @@ package it.testori.thip.base.generale.api;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Vector;
 
 import javax.ws.rs.core.Response.Status;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.thera.thermfw.base.Trace;
 import com.thera.thermfw.common.ErrorMessage;
 import com.thera.thermfw.persist.Factory;
 import com.thera.thermfw.rs.errors.ErrorUtils;
 
+import it.testori.thip.base.articolo.YArticoloDatiMagaz;
 import it.testori.thip.easycheck.PezzaGreggiaField;
 import it.testori.thip.easycheck.PezzaLavorata;
+import it.thera.thip.base.azienda.Azienda;
+import it.thera.thip.produzione.ordese.AttivitaEsecMateriale;
+import it.thera.thip.produzione.ordese.AttivitaEsecutiva;
+import it.thera.thip.produzione.ordese.AttivitaEsecutivaTM;
 
 /**
  * <p></p>
@@ -76,7 +83,23 @@ public class EasyCheckService {
 		}else {
 			PezzaGreggiaField pezza = PezzaGreggiaField.fromPayload(payload, errors);
 			if(errors.isEmpty()) {
-
+				String bollaLavorazione = pezza.getProductionOrder();
+				AttivitaEsecutiva atvEsec = leggiAtvEsec(bollaLavorazione);
+				if(atvEsec != null 
+						&& atvEsec.getMateriali().size() > 0
+						&& ((AttivitaEsecMateriale) atvEsec.getMateriali().get(0)).getArticolo().getArticoloDatiMagaz().getCodAutLotProd()  == YArticoloDatiMagaz.PEZZE) {
+					AttivitaEsecMateriale materiale = (AttivitaEsecMateriale) atvEsec.getMateriali().get(0);
+					
+				}else {
+					if(atvEsec == null)
+						errors.add(new ErrorMessage("BAS0000078","Non e' stata trovata nessuna attivita con codice :"+bollaLavorazione));
+					else if(atvEsec.getMateriali().size() == 0)
+						errors.add(new ErrorMessage("BAS0000078","Non e' stata trovato nessun materiale per l'attivita :"+bollaLavorazione));
+					else if(((AttivitaEsecMateriale) atvEsec.getMateriali().get(0)).getArticolo().getArticoloDatiMagaz().getCodAutLotProd() != YArticoloDatiMagaz.PEZZE)
+						errors.add(new ErrorMessage("BAS0000078","Il materiale dell'attivita non e' una pezza"));
+					
+					status = Status.INTERNAL_SERVER_ERROR;
+				}
 			}else {
 				status = Status.BAD_REQUEST;
 			}
@@ -86,5 +109,21 @@ public class EasyCheckService {
 		response.put("response", result);
 		response.put("status", status);
 		return response;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static AttivitaEsecutiva leggiAtvEsec(String bollaLavorazione) {
+		String where = AttivitaEsecutivaTM.ID_AZIENDA + " = '" + Azienda.getAziendaCorrente() + "' AND " +
+				AttivitaEsecutivaTM.NUM_RITORNO + " = '" + bollaLavorazione + "'";
+		try {
+			Vector vect = AttivitaEsecutiva.retrieveList(where, "", true);
+			if (!vect.isEmpty()) {
+				return (AttivitaEsecutiva) vect.firstElement();
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace(Trace.excStream);
+		}
+		return null;
 	}
 }
