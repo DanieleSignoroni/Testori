36b3591defe043900d6f4bdd48d7d4a9bc1d60ba
Daniele aka COBRA <daniele.signoroni@softre.it>
2025-12-05 09:41:46 +0100

    clean main

--- 5.1/websrc/it/testori/thip/vendite/ordineVE/YAllegatiOrdVenRigPrm.js
+++
@@ -1,8 +0,0 @@
-function YAllegatiOrdVenRigPrmOL() {
-	var mode = document.forms[0].thMode.value;
-	if (mode === "NEW") {
-		document.forms[0].AnnoDocumento.value = parent.opener.document.forms[0].AnnoOrdine.value;
-		document.forms[0].NumeroDocumento.value = parent.opener.document.forms[0].NumeroOrdine.value;
-		document.forms[0].NumeroRigaDocumento.value = parent.opener.document.forms[0].RigaOrdine.value;
-	}
-}
