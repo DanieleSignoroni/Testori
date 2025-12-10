6009a9fa3aae43acd4d9a95b5334401adcf7b677
Daniele aka COBRA <daniele.signoroni@softre.it>
2025-12-10 14:47:02 +0100

    dsds

--- 5.1/src/it/testori/thip/whytex/ImportDatiCicloWhytex.java
+++ 5.1/src/it/testori/thip/whytex/ImportDatiCicloWhytex.java
@@ -2,11 +2,21 @@ package it.testori.thip.whytex;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.Iterator;
 
 import com.thera.thermfw.base.Trace;
+import com.thera.thermfw.persist.KeyHelper;
 
+import it.thera.thip.base.azienda.Azienda;
+import it.thera.thip.base.generale.PersDatiGen;
 import it.thera.thip.cs.CSVFile;
 import it.thera.thip.cs.CSVRiga;
+import it.thera.thip.datiTecnici.modpro.AttivitaProdRisorsa;
+import it.thera.thip.datiTecnici.modpro.AttivitaProduttiva;
+import it.thera.thip.datiTecnici.modpro.ModelloProduttivo;
+import it.thera.thip.datiTecnici.modpro.ModproEsplosione;
 
 /**
  * Importazione dei dati relativi al ciclo di produzione di un articolo.<br>
@@ -61,15 +71,52 @@ public class ImportDatiCicloWhytex extends ImportFileWhytex {
 		return ok;
 	}
 
+	@SuppressWarnings("rawtypes")
 	@Override
 	public boolean elaboraRiga(CSVRiga rigaCSV, Integer numeroRiga) {
 		boolean ok = true;
 		try {
-			String idArticolo = rigaCSV.getValore(0);
+			String idArticoloPadre = rigaCSV.getValore(0);
 			String idOperazione = rigaCSV.getValore(1);
 			String tempo = rigaCSV.getValore(2);
+			String risorsa = null;
 			if(rigaCSV.getValori().size() > 3) {
-				String risorsa = rigaCSV.getValore(3);
+				risorsa = rigaCSV.getValore(3);
+			}
+			ModelloProduttivo modelloProduttivo = null;
+			try {
+				char[] tipi = new char[3]; 
+				tipi[0] = ModelloProduttivo.PRODUZIONE;
+				tipi[1] = ModelloProduttivo.ATTREZZAGGIO;
+				tipi[2] = ModelloProduttivo.RILAVORAZIONE;
+				modelloProduttivo = ModproEsplosione.trovaModelloProduttivo(Azienda.getAziendaCorrente(),
+						idArticoloPadre, PersDatiGen.getCurrentPersDatiGen().getIdStabilimento(), null, null, ModelloProduttivo.GENERICO, tipi);
+				if(modelloProduttivo != null) {
+					BigDecimal tempoUnitarioBD = stringToBigDecimal(tempo);
+					Iterator iterAtvProd = modelloProduttivo.getAttivita().iterator();
+					while(iterAtvProd.hasNext()) {
+						AttivitaProduttiva attivita = (AttivitaProduttiva) iterAtvProd.next();
+						if(attivita.getIdOperazione().equals(idOperazione)) {
+							if(risorsa == null) {
+								settaTempoUnitarioRisorsaPrincipale(attivita, tempoUnitarioBD);
+							}else {
+								settaTempoUnitarioRisorsa(attivita,risorsa, tempoUnitarioBD);
+							}
+						}
+						int rc = attivita.save();
+						if(rc > 0) {
+							output.println("attivita "+KeyHelper.formatKeyString(attivita.getKey())+" aggiornata correttamente, rc = "+rc);
+							numRigheInserite++;
+						}else {
+							ok = false;
+							numRigheConErrori++;
+						}
+					}
+				}else{
+					output.println("Per l'articolo : "+idArticoloPadre+" non e' stato trovato nessun modello generico di produzione");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace(Trace.excStream);
 			}
 		}catch (Exception e) {
 			e.printStackTrace(Trace.excStream);
@@ -80,6 +127,30 @@ public class ImportDatiCicloWhytex extends ImportFileWhytex {
 		if (ok)
 			numRigheInserite++;
 		return ok;
+	}
+
+	@SuppressWarnings("rawtypes")
+	protected void settaTempoUnitarioRisorsa(AttivitaProduttiva attivitaProduttiva, String idRisorsa, BigDecimal tempoUnitario) {
+		Iterator iterRisorse = attivitaProduttiva.getRisorse().iterator();
+		while(iterRisorse.hasNext()) {
+			AttivitaProdRisorsa risorsa = (AttivitaProdRisorsa) iterRisorse.next();
+			if(risorsa.getRRisorsa().equals(idRisorsa)) {
+				risorsa.setTempoUnitario(tempoUnitario);
+				risorsa.setTempoFisso(null);
+			}
+		}
+	}
+
+	@SuppressWarnings("rawtypes")
+	public void settaTempoUnitarioRisorsaPrincipale(AttivitaProduttiva attivitaProduttiva, BigDecimal tempoUnitario) {
+		Iterator iterRisorse = attivitaProduttiva.getRisorse().iterator();
+		while(iterRisorse.hasNext()) {
+			AttivitaProdRisorsa risorsa = (AttivitaProdRisorsa) iterRisorse.next();
+			if(risorsa.getPolConsRisorse() == AttivitaProduttiva.PRINCIPALE) {
+				risorsa.setTempoUnitario(tempoUnitario);
+				risorsa.setTempoFisso(null);
+			}
+		}
 	}
 
 	@Override
