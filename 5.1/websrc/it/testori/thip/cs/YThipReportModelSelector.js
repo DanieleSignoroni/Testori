538c01288c760578d0aeb64d1beb600f4cb9fb9c
Daniele aka COBRA <daniele.signoroni@softre.it>
2026-01-29 14:28:05 +0100

    ds

--- 5.1/websrc/it/testori/thip/cs/YThipReportModelSelector.js
+++ 5.1/websrc/it/testori/thip/cs/YThipReportModelSelector.js
@@ -6,20 +6,20 @@ abilitaCheckGesDocumentale = function(reportModel) {
 	if (reportModel != "" && ClassHdrName.includes('ReportDdtBollaBatch')) {
 		if (hasSSDAttivo[reportModel] == 'Y') {
 			document.forms[0].AllegatiDigitaliCom.checked = true;
-			document.forms[0].AllegatiDigitaliComLbl.style.display = displayBlock;
+			document.getElementById('AllegatiDigitaliComLbl').style.display = displayBlock;
 			document.forms[0].AllegatiDigitaliCom.style.display = displayBlock;
 		} else {
 			document.forms[0].AllegatiDigitaliCom.checked = false;
-			document.forms[0].AllegatiDigitaliComLbl.style.display = displayNone;
+			document.getElementById('AllegatiDigitaliComLbl').style.display = displayNone;
 			document.forms[0].AllegatiDigitaliCom.style.display = displayNone;
 		}
 		if (hasSSDUtente[reportModel] == 'Y') {
 			document.forms[0].AllegatiDigitaliCom.checked = true;
-			document.forms[0].AllegatiDigitaliComLbl.style.display = displayBlock;
+			document.getElementById('AllegatiDigitaliComLbl').style.display = displayBlock;
 			document.forms[0].AllegatiDigitaliCom.style.display = displayBlock;
 		} else {
 			document.forms[0].AllegatiDigitaliCom.checked = false;
-			document.forms[0].AllegatiDigitaliComLbl.style.display = displayNone;
+			document.getElementById('AllegatiDigitaliComLbl').style.display = displayNone;
 			document.forms[0].AllegatiDigitaliCom.style.display = displayNone;
 		}
 	}
